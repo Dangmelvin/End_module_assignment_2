c4bb1893829c98804956d5c55f8e411553b39e33
Loc Nguyen <loc.nguyenhuu@xendit.co>
2024-03-14 14:31:58 +0700

    Update code

--- client_code.py
+++ client_code.py
@@ -5,6 +5,13 @@ import xml.etree.ElementTree as ET
 from cryptography.fernet import Fernet
 import os
 
+SERIALIZE_FORMAT = {
+    "plaintext": 0,
+    "binary": 1,
+    "json": 2,
+    "xml": 3
+}
+
 def get_key_from_file(filename="GroupB.key"):
     if not os.path.exists(filename):
         raise Exception(f"{filename} file not found")
@@ -23,13 +30,8 @@ def encrypt_file(file_path):
         encrypted_data = cipher.encrypt(plaintext)
     return encrypted_data
 
-def send_data_to_server(data, filename, pickling_format):
-    HOST = '127.0.0.1'
-    PORT = 6868
 
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((HOST, PORT))
-
+def create_message_boundary(data, pickling_format):
     # Send pickled data based on selected format
     if pickling_format == "binary":
         pickled_data = pickle.dumps(data)
@@ -41,14 +43,48 @@ def send_data_to_server(data, filename, pickling_format):
             child = ET.SubElement(root, key)
             child.text = value
         pickled_data = ET.tostring(root)
+    elif pickling_format == 'plaintext':
+        pickled_data = data
     else:
         raise ValueError("Invalid pickling format")
-    
-    client.sendall(pickled_data)
+
+    # calculate header
+    msg_len = len(pickled_data)
+    msg_format = SERIALIZE_FORMAT[pickling_format]
+
+    header = msg_len.to_bytes(4, byteorder='big') + msg_format.to_bytes(1, byteorder='big')
+
+    return header + pickled_data
+
+
+def send_data_to_server(data, filename, pickling_format):
+    HOST = '127.0.0.1'
+    PORT = 6868
+
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
+
+    # Send pickled data based on selected format
+    # if pickling_format == "binary":
+    #     pickled_data = pickle.dumps(data)
+    # elif pickling_format == "json":
+    #     pickled_data = json.dumps(data).encode('utf-8')
+    # elif pickling_format == "xml":
+    #     root = ET.Element("data")
+    #     for key, value in data.items():
+    #         child = ET.SubElement(root, key)
+    #         child.text = value
+    #     pickled_data = ET.tostring(root)
+    # else:
+    #     raise ValueError("Invalid pickling format")
+
+    _send_data_1 = create_message_boundary(data, pickling_format)
+    client.sendall(_send_data_1)
 
     # Encrypt and send text file
     encrypted_data = encrypt_file(filename)
-    client.sendall(encrypted_data)
+    _send_data_2 = create_message_boundary(encrypted_data, "plaintext")
+    client.sendall(_send_data_2)
 
     client.close()
 
--- server_code.py
+++ server_code.py
@@ -5,6 +5,16 @@ import xml.etree.ElementTree as ET
 from cryptography.fernet import Fernet
 import os
 
+SERIALIZE_FORMAT = {
+    "plaintext": 0,
+    "binary": 1,
+    "json": 2,
+    "xml": 3
+}
+
+HEADER_LENGTH = 4
+MSG_FORMAT_LENGTH = 1
+
 def generate_key_and_save_to_file(filename="GroupB.key"):
     # check key exist before generating
     if os.path.exists(filename):
@@ -29,43 +39,64 @@ def save_data(data, filename):
     with open(filename, "wb") as f:
         pickle.dump(data, f)
 
+
+def process_msg(msg, msg_format):
+    if msg_format == SERIALIZE_FORMAT["binary"]:
+        received_dict = pickle.loads(msg)
+    elif msg_format == SERIALIZE_FORMAT["json"]:
+        received_dict = json.loads(msg.decode('utf-8'))
+    elif msg_format == SERIALIZE_FORMAT["xml"]:
+        root = ET.fromstring(msg)
+        received_dict = {}
+        for child in root:
+            received_dict[child.tag] = child.text
+    elif msg_format == SERIALIZE_FORMAT["plaintext"]:
+        received_dict = decrypt_file(msg.decode('utf-8'))
+        with open("GroupB_sample_textfile_Decryp.txt", "wb") as f:
+            f.write(received_dict)
+    else:
+        raise ValueError("Invalid pickling format")
+    return received_dict
+
+
 def handle_client(conn, addr, pickling_format, save_to_file=False):
     print(f"Connection from {addr} has been established.")
     
     # Receive pickled data based on selected format
     data = b''
+
     while True:
-        chunk = conn.recv(4096)
-        if not chunk:
+        # chunk = conn.recv(4096)
+        # if not chunk:
+        #     break
+        # data += chunk
+
+        header = conn.recv(HEADER_LENGTH)
+        if not header:
+            # no header received
             break
-        data += chunk
-    
-    if pickling_format == "binary":
-        received_dict = pickle.loads(data)
-    elif pickling_format == "json":
-        received_dict = json.loads(data.decode('utf-8'))
-    elif pickling_format == "xml":
-        root = ET.fromstring(data)
-        received_dict = {}
-        for child in root:
-            received_dict[child.tag] = child.text
-    else:
-        raise ValueError("Invalid pickling format")
-    
-    # Receive and decrypt text file
-    encrypted_data = b''
-    while True:
-        chunk = conn.recv(4096)
-        if not chunk:
-            break
-        encrypted_data += chunk
-    
-    decrypted_data = decrypt_file(encrypted_data)
-    with open("GroupB_sample_textfile_Decryp.txt", "wb") as f:
-        f.write(decrypted_data)
-    
-    if save_to_file:
-        save_data(received_dict, "received_data.pkl")
+
+        msg_len = int.from_bytes(header[0:HEADER_LENGTH], byteorder='big')
+        file_format_bytes = conn.recv(MSG_FORMAT_LENGTH)
+        file_format = int.from_bytes(file_format_bytes, byteorder='big')
+
+        print(f"msg_len = {msg_len}, file_format = {file_format}")
+
+        chunks = []
+        bytes_received = 0
+        while bytes_received < msg_len:
+            chunk = conn.recv(min(4096, msg_len - bytes_received))
+            if not chunk:
+                break
+            chunks.append(chunk)
+            bytes_received += len(chunk)
+
+        data = b"".join(chunks)
+        received_dict = process_msg(data, file_format)
+        if save_to_file:
+            save_data(received_dict, "received_data.pkl")
+
+
     
     print("Data and file received successfully.")
 
